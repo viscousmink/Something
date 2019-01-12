0254456bf27734a6a7c4f602f0c8a51e67802767
viscousmink <mitchell.klingler99@gmail.com>
2019-01-11 19:12:21 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
+
+var num = 6;
+var Cities = [];
+
 function setup() {
   // put setup code here
 	createCanvas(500, 500);
+	for(var i = 0; i<num; i++) {
+		Cities = createVector(floor(random(width)), floor(random(height)));
+	}
 }
 
 function draw() {
   // put drawing code here
 	background(0);
 	fill(255);
-	ellipse(50,50,80,80);
+	for(var i = 0; i<num; i++) {
+		ellipse(Cities[i].x, Cities[i].y, 10, 10);
+	}
 }
