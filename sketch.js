61e204eac6fb2331b1551667ad22df954f3b64cd
viscousmink <mitchell.klingler99@gmail.com>
2019-01-11 19:23:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,29 @@
 
 var num = 6;
 var Cities = [];
+var best = [];
+var order = [];
+var tracker;
+var min = Infinity;
+
+change = function() {
+	for(var i = 0; i<num; i++) {
+		var tmp = order[i];
+		var tmp2 = floor(random(num));
+		order[i] = order[tmp2];
+		order[tmp2] = tmp;
+	}
+}
 
 function setup() {
   // put setup code here
 	createCanvas(500, 500);
 	for(var i = 0; i<num; i++) {
 		Cities[i] = createVector(floor(random(width)), floor(random(height)));
+	}
+	for(var i = 0; i<num; i++) {
+		order[i] = i;
+		best[i] = i;
 	}
 }
 
@@ -16,5 +33,17 @@ function draw() {
 	fill(255);
 	for(var i = 0; i<num; i++) {
 		ellipse(Cities[i].x, Cities[i].y, 10, 10);
+	}
+	for(var i = 0; i<num-1; i++) {
+		stroke(255);
+		line(Cities[best[i]].x, Cities[best[i]].y, Cities[best[i+1]].x, Cities[best[i+1]].y);
+		stroke(50);
+		line(Cities[order[i]].x, Cities[order[i]].y, Cities[order[i+1]].x, Cities[order[i+1]].y);
+	}
+	var distance;
+	distance = dist(Cities[order[i]].x, Cities[order[i]].y, Cities[order[i+1]].x, Cities[order[i+1]].y);
+	if(distance < min) {
+		min = distance;
+		best = order.slice();
 	}
 }
